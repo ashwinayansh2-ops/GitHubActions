156f2ede3c406d03e0be7e382c5a0894b007be83
ashwinayansh2-ops <ashwinayansh2@gmail.com>
2026-06-29 01:16:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-  ami="ami-0f88e80871fd81e91"
+  ami="ami-08f44e8eca9095668"
   instance_type = "t2.micro"
   tags={
     Name = "dev-instance"
